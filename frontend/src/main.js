94d8e393ac300f2b1c0d87d50a246a7a7763f701
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-01-21 23:53:42 +0900

    destroy

--- frontend/src/main.js
+++
@@ -1,8 +0,0 @@
-// src/main.js
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'  // Vue Router 추가
-
-createApp(App)
-  .use(router)  // Vue Router 적용
-  .mount('#app')
