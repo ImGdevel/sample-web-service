c24a2a2dfedc5f64a31f47539b1f5a96a5c97a27
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-01-29 21:37:51 +0900

    frontend/createPost

--- frontend/src/views/CreatePost.vue
+++ frontend/src/views/CreatePost.vue
@@ -1,87 +1,105 @@
 <!-- views/CreatePost.vue -->
 <template>
-    <main class="create-post-container">
-      <h1>게시글 작성</h1>
-      <!-- 게시글 작성 양식 등을 추가하세요 -->
-      <form @submit.prevent="submitPost" class="post-form">
-        <div class="form-group">
-          <label for="title">제목:</label>
-          <input type="text" class="form-control" v-model="post.title" required>
-        </div>
+  <main class="create-post-container">
+    <h1>게시글 작성</h1>
+    <!-- Post creation form -->
+    <form @submit.prevent="submitPost" class="post-form">
+      <!-- Title input -->
+      <div class="form-group">
+        <label for="title">제목:</label>
+        <input type="text" class="form-control" v-model="post.title" required>
+      </div>
 
-        <div class="form-group">
-          <label for="content">내용:</label>
-          <textarea class="form-control" v-model="post.content" required></textarea>
-        </div>
+      <!-- Author input -->
+      <div class="form-group">
+        <label for="author">작성자:</label>
+        <input type="text" class="form-control" v-model="post.member.userId" required>
+      </div>
+
+      <!-- Content textarea -->
+      <div class="form-group">
+        <label for="content">내용:</label>
+        <textarea class="form-control" v-model="post.content" required></textarea>
+      </div>
+
+      <!-- Submit button -->
+      <button type="submit" class="btn btn-primary">게시글 작성</button>
+    </form>
+  </main>
+</template>
+
   
-        <button type="submit" class="btn btn-primary">게시글 작성</button>
-      </form>
-    </main>
-  </template>
-  
-  <script>
-  export default {
-    name: 'CreatePost',
-    data() {
-      return {
-        post: {
-          title: '',
-          author: '',
-          content: '',
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'CreatePost',
+  data() {
+    return {
+      post: {
+        title: '',
+        member: {
+          userId: '',
         },
-      };
-    },
-    methods: {
-      submitPost() {
-        // 게시글을 서버에 저장하는 로직을 추가하세요
-        console.log('게시글 작성:', this.post);
-        // 작성 후 홈으로 이동 또는 다른 동작을 수행할 수 있습니다.
-        this.$router.push({ name: 'PostView' });
+        content: '',
       },
+    };
+  },
+
+  methods: {
+    submitPost() {
+      console.log(this.post);
+      axios.post(process.env.VUE_APP_API + '/api/posts', this.post )
+      .then(response => {
+          console.log('게시글이 성공적으로 생성되었습니다:', response.data);
+          this.$router.push({ name: 'PostView' });
+        })
+        .catch(error => {
+          console.error('게시글 생성 오류:', error);
+        });
     },
-  };
-  </script>
+  },
+};
+</script>
   
-  <style lang="scss" scoped>
+<style lang="scss" scoped>
+.create-post-container {
+  max-width: 600px;
+  padding: 20px;
+  text-align: left;
+}
 
+.post-form {
+  background-color: #fff;
+  padding: 20px;
+}
 
-  .create-post-container {
-    max-width: 600px;
-    padding: 20px;
-    text-align: left;
-  }
-  
-  .post-form {
-    background-color: #fff;
-    padding: 20px;
-  }
-  
-  .form-group {
-    margin-bottom: 20px;
-  }
-  
-  label {
-    display: block;
-    font-weight: bold;
-    margin-bottom: 8px;
-  }
-  
-  .input-control {
-    width: 100%;
-    padding: 10px;
-    font-size: 16px;
-    border: 1px solid #ddd;
-    border-radius: 4px;
-    outline-color: black;
-  }
-  
-  button {
-    padding: 10px 20px;
-    background-color: #007BFF;
-    color: #fff;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-  }
-  </style>
+.form-group {
+  margin-bottom: 20px;
+}
+
+label {
+  display: block;
+  font-weight: bold;
+  margin-bottom: 8px;
+}
+
+.input-control {
+  width: 100%;
+  padding: 10px;
+  font-size: 16px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  outline-color: black;
+}
+
+button {
+  padding: 10px 20px;
+  background-color: #007BFF;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+</style>
   
--- frontend/src/views/PostView.vue
+++ frontend/src/views/PostView.vue
@@ -1,114 +1,123 @@
 <!-- views/PostView.vue -->
 <template>
-    <main>
-      <h1>Post</h1>
-      <div>
-        <!-- 게시글 리스트 -->
-        <table class="post-table">
-          <thead>
-            <tr>
-              <th>번호</th>
-              <th>제목</th>
-              <th>작성자</th>
-              <th>작성일</th>
-              <th>조회수</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="post in posts" :key="post.id"  @click="goToPostDetail(post.id)">
-              <td>{{ post.id }}</td>
-              <td>{{ post.title }}</td>
-              <td>{{ post.member.username }}</td>
-              <td>{{ formatTime(post.createdAt) }}</td>
-              <td>{{ post.views }}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </main>
-  </template>
+  <main>
+    <h1>Post</h1>
+    <div>
+      <!-- 게시글 리스트 -->
+      <table class="post-table">
+        <thead>
+          <tr>
+            <th>번호</th>
+            <th>제목</th>
+            <th>작성자</th>
+            <th>작성일</th>
+            <th>조회수</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="post in posts" :key="post.id" @click="goToPostDetail(post.id)">
+            <td>{{ post.id }}</td>
+            <td>{{ post.title }}</td>
+            <td>{{ post.member.username }}</td>
+            <td>{{ formatTime(post.createdAt) }}</td>
+            <td>{{ post.views }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <br>
+    <div>
+      <button @click="goToCreatePost()">
+        게시글 작성
+      </button>
+    </div>
+  </main>
+</template>
   
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    name: 'PostView',
-    data() {
-      return {
-        posts: [],
-      };
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'PostView',
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  mounted() {
+    this.fetchPosts();
+  },
+  methods: {
+    fetchPosts() {
+      axios.get(process.env.VUE_APP_API + '/api/posts')
+        .then(response => {
+
+          this.posts = response.data;
+        })
+        .catch(error => {
+          console.error('API 호출 중 에러:', error);
+        });
     },
-    mounted() {
-      this.fetchPosts();
-    },
-    methods: {
-      fetchPosts() {
-        axios.get(process.env.VUE_APP_API + '/api/posts')
-          .then(response => {
-            
-            this.posts = response.data;
-          })
-          .catch(error => {
-            console.error('API 호출 중 에러:', error);
-          });
-      },
-      formatTime(timestamp) {
-      if(timestamp === undefined) return ""; 
+    formatTime(timestamp) {
+      if (timestamp === undefined) return "";
       const date = new Date(timestamp);
       return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
     },
-      goToPostDetail(postId) {
-            // 클릭한 게시글의 ID를 사용하여 상세 페이지로 이동
-            this.$router.push({ name: 'PostDetail', params: { id: postId } });
-        },
+    goToPostDetail(postId) {
+      // 클릭한 게시글의 ID를 사용하여 상세 페이지로 이동
+      this.$router.push({ name: 'PostDetail', params: { id: postId } });
     },
-  };
-  </script>
+    goToCreatePost() {
+      this.$router.push({ name: 'CreatePost' });
+    },
+  },
+};
+</script>
   
-  <style lang="scss" scoped>
-  .post-table {
-    width: 100%;
-    border-collapse: collapse;
-  }
-  
-  .post-table th,
-  .post-table td {
-    padding: 10px;
-    text-align: left;
-  }
-  
-  .post-table th {
-    background-color: #f2f2f2;
-    border-bottom: 2px solid #373737;
-  }
-  
-  .post-table tbody tr:hover {
-    background-color: #ddd;
-  }
-  
-  .post-table th:first-child,
-  .post-table td:first-child {
-    width: 5%;
-  }
-  
-  .post-table th:nth-child(2),
-  .post-table td:nth-child(2) {
-    width: 40%;
-  }
-  
-  .post-table th:nth-child(3),
-  .post-table td:nth-child(3) {
-    width: 10%;
-  }
-  
-  .post-table th:nth-child(4),
-  .post-table td:nth-child(4) {
-    width: 15%;
-  }
-  
-  .post-table th:last-child,
-  .post-table td:last-child {
-    width: 10%;
-  }
-  </style>
+<style lang="scss" scoped>
+.post-table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+.post-table th,
+.post-table td {
+  padding: 10px;
+  text-align: left;
+}
+
+.post-table th {
+  background-color: #f2f2f2;
+  border-bottom: 2px solid #373737;
+}
+
+.post-table tbody tr:hover {
+  background-color: #ddd;
+}
+
+.post-table th:first-child,
+.post-table td:first-child {
+  width: 5%;
+}
+
+.post-table th:nth-child(2),
+.post-table td:nth-child(2) {
+  width: 40%;
+}
+
+.post-table th:nth-child(3),
+.post-table td:nth-child(3) {
+  width: 10%;
+}
+
+.post-table th:nth-child(4),
+.post-table td:nth-child(4) {
+  width: 15%;
+}
+
+.post-table th:last-child,
+.post-table td:last-child {
+  width: 10%;
+}
+</style>
   
