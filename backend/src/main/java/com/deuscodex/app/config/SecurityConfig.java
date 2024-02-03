10c46a2b628a3342b1b95da3e72e96d98c28790e
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-02-03 22:42:17 +0900

    Join - Security To Passward

--- backend/src/main/java/com/deuscodex/app/config/SecurityConfig.java
+++ backend/src/main/java/com/deuscodex/app/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.deuscodex.app.config;
 
+import org.aspectj.weaver.BCException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,13 +13,17 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig{
 
     @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
     public SecurityFilterChain fitterChain(HttpSecurity http) throws Exception{
 
         http.authorizeHttpRequests((auth)-> auth
-                .requestMatchers("/", "/login", "/join").permitAll()
+                .requestMatchers("/", "/login", "/join", "/swagger-ui/**").permitAll()
                 .requestMatchers("/admin").hasRole("ADMIN")
                 .requestMatchers("/api/**").hasAnyRole("ADMIN", "USER")
-                .anyRequest().denyAll()
+                .anyRequest().permitAll()
         );
 
         http.formLogin((auth)->auth
--- backend/src/main/java/com/deuscodex/app/mapper/MemberMapper.java
+++ backend/src/main/java/com/deuscodex/app/mapper/MemberMapper.java
@@ -25,4 +25,5 @@ public class MemberMapper {
                 .email(memberDTO.getEmail())
                 .build();
     }
+
 }
