cc245dc4da1a6557a80f9179ad76bd572864f8a5
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-02-04 20:34:05 +0900

    Logger(Logback)

--- backend/src/main/java/com/deuscodex/app/controller/LoginController.java
+++ backend/src/main/java/com/deuscodex/app/controller/LoginController.java
@@ -1,6 +1,8 @@
 package com.deuscodex.app.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -16,8 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/login")
 public class LoginController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
+
     @GetMapping
     public String loginForm() {
+        LOGGER.info("Login From Call");
         return "login";
     }
 
@@ -29,7 +34,8 @@ public class LoginController {
     ) {
         // 여기에서 받은 데이터를 이용하여 로그인 처리 또는 필요한 작업 수행
         // ...
-
+        LOGGER.info("FROM");
+        LOGGER.info("username = {} password = {}" ,username, password);
         // 예제: 받은 데이터를 모델에 추가하여 Thymeleaf에서 사용할 수 있도록 함
         model.addAttribute("username", username);
         model.addAttribute("password", password);
