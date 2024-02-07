927b12d33be6150275cff3a78213a7b84c4dcdec
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-02-07 16:44:47 +0900

    Add

--- backend/src/main/java/com/deuscodex/app/config/SecurityConfig.java
+++ backend/src/main/java/com/deuscodex/app/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.deuscodex.app.config;
 import org.aspectj.weaver.BCException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,14 +21,15 @@ public class SecurityConfig{
     public SecurityFilterChain fitterChain(HttpSecurity http) throws Exception{
 
         http.authorizeHttpRequests((auth)-> auth
-                .requestMatchers("/", "/login", "/join", "/swagger-ui/**").permitAll()
+                .requestMatchers("/", "/login", "/join", "/post").permitAll()
+                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**").permitAll()
                 .requestMatchers("/admin").hasRole("ADMIN")
                 .requestMatchers("/api/**").hasAnyRole("ADMIN", "USER")
                 .anyRequest().permitAll()
             )
             .formLogin((form)->form
                 .loginPage("/login")
-                .loginProcessingUrl("/login")
+                .loginProcessingUrl("/loginProc")
                 .permitAll()
             )
             .csrf(auth -> auth.disable()
